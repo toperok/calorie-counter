88f7ef5ac76133b0364f78d939a1c151b47fabe3
Daniil Malashenko <daniil2007mmm@gmail.com>
2026-04-24 22:45:37 +0300

    Config update.

--- configs/config.py
+++ configs/config.py
@@ -1,13 +1,15 @@
 class Config:
     SEED = 42
 
+    # Models used
     TEXT_MODEL_NAME = "DeepPavlov/rubert-base-cased"
     IMAGE_MODEL_NAME = "tf_efficientnet_b0"
 
-
+    # Trainable layers
     TEXT_MODEL_UNFREEZE = "encoder.layer.11|pooler"
     IMAGE_MODEL_UNFREEZE = "blocks.6|conv_head|bn2"
 
+    # Hyperparameters 
     BATCH_SIZE = 32
     TEXT_LR = 3e-5
     IMAGE_LR = 1e-4
@@ -19,11 +21,12 @@ class Config:
     
     OUTPUT_DIM = 1 
 
+    # PATHS
     NEDA_PATH = "data/dish.csv"
     CSV_PATH = "data/dish_eda.csv"
     INGR_CSV_PATH = "data/ingredients.csv"
     IMG_DIR = "data/images"
-    SAVE_PATH = "best_model.pth"
+    SAVE_PATH = "best_model.pth"    # best model weight
     TRAIN_DF_PATH = "data/train_df.csv"
     VAL_DF_PATH = "data/val_df.csv"
     TEST_DF_PATH = "data/test_df.csv"
